33311b0d9b8289ed34a8f4668fba70dbd85162f0
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 18:42:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,26 +9,10 @@ function nowServing(deliLine){
   }
   var temp = deliLine[0]
   deliLine.shift()
-  return temp
+  return `Currently serving ${temp}`
 }
-//     it('adds a person to the line', () => {
-//       expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
-//       expect(katzDeli).toEqual(['Ada']);
-//     });
 
-//     it('appends the person the end of the line if there are already people on it', () => {
-//       expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
-//       expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
-//     });
 
-//     it("properly handles multiple people being added", () => {
-//       takeANumber(katzDeli, 'Ada');
-//       takeANumber(katzDeli, 'Grace');
-//       takeANumber(katzDeli, 'Kent');
-
-//       expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
-//     });
-//   });
 
 //   describe('nowServing', () => {
 //     it('returns the line is empty when no one is on line', () => {
