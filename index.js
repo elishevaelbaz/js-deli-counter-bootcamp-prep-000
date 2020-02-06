6e7fb6c4ac4f1c76b29baf8842633e85aeb29f46
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 18:40:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(deliLine){
   if(deliLine.length === 0){
-    return "There is nobody waiting to be served"
+    return "There is nobody waiting to be served!"
   }
   var temp = deliLine[i]
   deliLine.shift()
